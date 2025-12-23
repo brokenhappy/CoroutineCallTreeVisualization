0a6923ec64e6d789a4ae770620e68cf822695f91
wout.werkman <wout.werkman@jetbrains.com>
2025-12-23 21:05:25 +0100

    Separate kotlinx coroutines integration from trackingCallStacks logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,4 @@ include("gradle-plugin")
 include("plugin-annotations")
 
 include("examples")
+include("tracked-call-tree-as-flow")
--- tracked-call-tree-as-flow/build.gradle.kts
+++ tracked-call-tree-as-flow/build.gradle.kts
@@ -1,11 +1,12 @@
-@file:OptIn(ExperimentalWasmDsl::class)
-
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.binary.compatibility.validator)
     `maven-publish`
+}
+
+dependencies {
+    commonMainApi(project(":plugin-annotations"))
+    commonMainApi(libs.kotlinx.coroutines.core)
 }
 
 kotlin {
