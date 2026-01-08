e6fb7a8bec5717099388c51e23d802e6fac008b5
wout.werkman <wout_werkman@hotmail.com>
2026-01-08 21:11:43 +0100

    Add forgotten Theme file

---
+++ call-tree-visualizer-gui/src/main/kotlin/com/woutwerkman/calltreevisualizer/gui/Theme.kt
@@ -0,0 +1,45 @@
+package com.woutwerkman.calltreevisualizer.gui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color(0xFFBB86FC),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6),
+    surface = Color(0xFF121212),
+    onSurface = Color.White,
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color(0xFF6200EE),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6),
+    surface = Color.White,
+    onSurface = Color.Black,
+    background = Color.White,
+    onBackground = Color.Black,
+)
+
+@Composable
+fun CallTreeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
