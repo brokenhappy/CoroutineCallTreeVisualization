03482866f0e035eda0cd382c4ffe88a7f91dff08
wout.werkman <wout_werkman@hotmail.com>
2026-01-08 22:03:29 +0100

    Optimize imports for SimplePluginComponentRegistrar.kt

--- compiler-plugin/src/com/woutwerkman/calltreevisualizer/SimplePluginComponentRegistrar.kt
+++ compiler-plugin/src/com/woutwerkman/calltreevisualizer/SimplePluginComponentRegistrar.kt
@@ -1,10 +1,9 @@
 package com.woutwerkman.calltreevisualizer
 
+import com.woutwerkman.calltreevisualizer.ir.SimpleIrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
-import com.woutwerkman.calltreevisualizer.ir.SimpleIrGenerationExtension
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
 
 class SimplePluginComponentRegistrar: CompilerPluginRegistrar() {
     override val supportsK2: Boolean
