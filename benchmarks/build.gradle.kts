0a5de55f057e1a13bdb981cf9db2da4b4feab272
wout.werkman <wout_werkman@hotmail.com>
2026-01-12 17:18:43 +0100

    Remove SNAPSHOTs from versions

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
 
 
 group = "com.woutwerkman"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "com.woutwerkman.calltreevisualizer"
-    version = "0.1.0-2.2.20-SNAPSHOT"
+    version = "0.0.1-2.2.20"
 
     repositories {
         mavenCentral()
